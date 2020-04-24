baf59329499e25f2501558cc552dc1fe13f05b2e
Angelo Petrolino <apetrolino3@gatech.edu>
2020-04-23 17:58:10 -0700

    json prototype

--- Model/Website Attachment/json_tester.py
+++ Model/Website Attachment/json_tester.py
@@ -7,10 +7,11 @@ from sklearn.externals import joblib
 model_1 = joblib.load(open('test_model.sav', 'rb'))
 
 while True:
-    if os.path.exists('mydata.json'):
+    if os.path.exists('inputData.json'):
 
-        inputData = json.load(open('mydata.json'))
-
+        inputData = json.load(open('inputData.json'))
+        ## Parse json for each industry
+        
         
 
         prediction = model_1.predict(np.array(inputData).reshape(1, -1))
@@ -19,6 +20,6 @@ while True:
 
         json.dump(prediction, open('outputData.json', 'w'))
 
-        os.remove('mydata.json')
+        os.remove('inputData.json')
     else:
         pass
